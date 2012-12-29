06acd6f12060bd17e3ff7084ee985d5d8315968e
dobabeswe <chololoy@gmail.com>
2012-12-29 11:51:35 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wandrr::Application.config.secret_token = 'bd949d38316a3df13f6c7a6f5c07492e5e3678b9e5df71c631ee41ba4c2e3dbc9c1b8318249d45e9a5ed61dbcc07981e71680ef823a1958bb92140168211f60f'
